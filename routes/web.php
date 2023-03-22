1e988349229d7b593015b9820150fd1a1e179d46
Softlabltd.com <freelancernishad123@gmail.com>
2023-03-22 22:39:39 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,86 @@ use App\Http\Controllers\NotificationsController;
 */
 
 
+
+Route::get('/akpay/test', function () {
+
+
+    $trnx_id = time();
+
+    $Apiurl = 'https://pg.ekpay.gov.bd/ekpaypg/v1';
+    $url = 'https://www.esspmtetulia.gov.bd';
+
+    $whitelistip = '198.54.114.109';
+   $req_timestamp = date('Y-m-d H:i:s');
+
+ $AKPAY_MER_REG_ID = 'tetulia_sechsheba_mer';
+$AKPAY_MER_PASS_KEY = 'sI8^Q2##';
+
+
+
+
+
+   $post = [
+      'mer_info' => [
+         "mer_reg_id" => $AKPAY_MER_REG_ID,
+         "mer_pas_key" => $AKPAY_MER_PASS_KEY
+      ],
+      "req_timestamp" => "$req_timestamp GMT+6",
+      "feed_uri" => [
+         "c_uri" => url("payment/cancel"),
+         "f_uri" => url("payment/fail"),
+         "s_uri" => url("payment/success")
+      ],
+      "cust_info" => [
+        "cust_email" => "",
+        "cust_id" => "1",
+        "cust_mail_addr" => "Address",
+        "cust_mobo_no" => "01909756552",
+        "cust_name" => "Customer Name"
+    ],
+      "trns_info" =>[
+        "ord_det" => 'sonod',
+        "ord_id" => "1",
+        "trnx_amt" => 10,
+        "trnx_currency" => "BDT",
+        "trnx_id" => "$trnx_id"
+    ],
+      "ipn_info" => [
+         "ipn_channel" => "3",
+         "ipn_email" => "freelancernishad123@gmail.com",
+         "ipn_uri" => "$url/api/ipn"
+      ],
+      "mac_addr" => "$whitelistip"
+   ];
+
+   // 148.163.122.80
+   $post = json_encode($post);
+
+
+   $ch = curl_init($Apiurl.'/merchant-api');
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+   $response = curl_exec($ch);
+   curl_close($ch);
+
+/*      echo '<pre>';
+   print_r($response); */
+
+
+  return   $response = json_decode($response);
+   $sToken =  $response->secure_token;
+
+
+   return "$Apiurl?sToken=$sToken&trnsID=$trnx_id";
+
+
+});
+
+
+
 Route::get('/apllication/document/{id}',[fronendController::class,'apllication_Document'])->name('apllication_Document');
 Route::get('/license/{id}',[fronendController::class,'license'])->name('license');
 
