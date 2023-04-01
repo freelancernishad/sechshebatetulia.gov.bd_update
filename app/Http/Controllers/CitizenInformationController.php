be8d71ef55566847a86c74c55561adc6f3ff1768
Softlabltd.com <freelancernishad123@gmail.com>
2023-04-01 10:16:09 +0600

    update

--- app/Http/Controllers/CitizenInformationController.php
+++ app/Http/Controllers/CitizenInformationController.php
@@ -60,8 +60,8 @@ class CitizenInformationController extends Controller
               $curl = curl_init();
 
               curl_setopt_array($curl, array(
-                CURLOPT_URL => 'https://api.porichoybd.com/sandbox-api/v2/verifications/autofill',
-                // CURLOPT_URL => 'https://api.porichoybd.com/api/v2/verifications/autofill',
+                // CURLOPT_URL => 'https://api.porichoybd.com/sandbox-api/v2/verifications/autofill',
+                CURLOPT_URL => 'https://api.porichoybd.com/api/v2/verifications/autofill',
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => '',
                 CURLOPT_MAXREDIRS => 10,
